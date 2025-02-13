6953060c48d52998f5f1db3f84f3146d05bb4a12
uttam715 <kumariuttam715@gmail.com>
2025-02-13 15:30:10 +0530

    Added search features and filter the todo by bar button and added deleted feature

--- src/App.js
+++ src/App.js
@@ -8,6 +8,27 @@ import "./App.css";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [tab, setTab] = useState("All");
+  const [updatedList, setUpdatedList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    setUpdatedList([...todoList]);
+  }, [todoList]);
+
+  useEffect(() => {
+    searchTodo();
+  }, [searchTerm]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  function searchTodo() {
+    const filteredTodos = todoList.filter((todo) =>
+      todo.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setUpdatedList(filteredTodos);
+  }
 
   function handleAddClick() {
     document.getElementById("createTodoModal").style.display = "block";
@@ -16,40 +37,90 @@ function App() {
   function addTodo(todo) {
     if (!todo.name) return;
     const id = generateUUID();
-    setTodoList([...todoList, { ...todo, id, status: false }]);
+    setTodoList([...todoList, { ...todo, id, status: true }]);
   }
 
-  function selectedTab(text){
+  function selectedTab(text) {
+    const updatedTodoList = [...todoList];
     setTab(text);
+    if (text === "Completed") {
+      const updated = updatedTodoList.filter((item) => item.status === true);
+      setUpdatedList(updated);
+    } else if (text === "Pending") {
+      const updated = updatedTodoList.filter((item) => item.status === false);
+      setUpdatedList(updated);
+    } else {
+      setUpdatedList([...todoList]);
+    }
+  }
+
+  function handleDeleteClick(task) {
+    const deletedTodo = todoList.findIndex((item) => task.id === item.id);
+    const updatedList = [...todoList];
+    updatedList.splice(deletedTodo, 1);
+    setTodoList(updatedList);
   }
 
   return (
     <div className="main border border-slate-300 border-solid main md:m-0 md:w-1/2 mx-4 p-2 relative rounded-lg w-full">
-        <div className="flex relative">
-          <input className="border border-slate-300 rounded-sm p-2 mb-8 w-full" type="text" value="" placeholder="search todo here"></input>
-          <button className="absolute bg-sky-300 border border-sky-300 font-bold px-5 py-2 right-0 rounded-sm text-l" onClick={handleAddClick}>
-            + Add Todos
-          </button>
-        </div>
-      
-        <div id="createTodoModal" className="hidden">
-          <TodoCreateModal addTodoItem={addTodo} />
-        </div>
-        <div className="max-h-[460px] min-h-[450px] overflow-auto">
-        {
-          todoList.length ?
-        <div className="todoIList">
-          {todoList.map((item) => (
-            <TodoItem task={item} key={item.id}></TodoItem>
-          ))}
-        </div> : <div className="m-auto mt-24 text-center text-lg w-48"> Nothing to create yet. Click on Create Todos to get started on your list.</div>
-        }
-        </div>
-        <div className="absolute border-sky-300 border-t border-slate-300 bottom-0 flex left-0 right-0">
-          <TodoBar tab={tab} text="All" design="border-r py-2 text-center w-1/3 rounded-bl-lg" selectedTab={selectedTab}/>
-          <TodoBar tab={tab} text="Pending" design="border-r py-2 text-center w-1/3 py-2" selectedTab={selectedTab}/>
-          <TodoBar tab={tab} text="Completed" design="py-2 text-center w-1/3 rounded-br-lg" selectedTab={selectedTab}/>
-        </div>
+      <div className="flex relative">
+        <input
+          className="border border-slate-300 rounded-sm p-2 mb-8 w-full"
+          type="text"
+          value={searchTerm}
+          placeholder="search todo here"
+          onChange={handleSearchChange}
+        ></input>
+        <button
+          className="absolute bg-sky-300 border border-sky-300 font-bold px-5 py-2 right-0 rounded-sm text-l"
+          onClick={handleAddClick}
+        >
+          + Add Todos
+        </button>
+      </div>
+
+      <div id="createTodoModal" className="hidden">
+        <TodoCreateModal addTodoItem={addTodo} />
+      </div>
+      <div className="max-h-[460px] min-h-[450px] overflow-auto">
+        {updatedList.length ? (
+          <div className="todoIList">
+            {updatedList.map((item) => (
+              <TodoItem
+                task={item}
+                key={item.id}
+                handleDeleteClick={handleDeleteClick}
+              ></TodoItem>
+            ))}
+          </div>
+        ) : (
+          <div className="m-auto mt-24 text-center text-lg w-48">
+            {" "}
+            Nothing to create yet. Click on Create Todos to get started on your
+            list.
+          </div>
+        )}
+      </div>
+      <div className="absolute border-sky-300 border-t border-slate-300 bottom-0 flex left-0 right-0">
+        <TodoBar
+          tab={tab}
+          text="All"
+          design="border-r py-2 text-center w-1/3 rounded-bl-lg border-slate-300"
+          selectedTab={selectedTab}
+        />
+        <TodoBar
+          tab={tab}
+          text="Pending"
+          design="border-r py-2 text-center w-1/3 py-2 border-slate-300"
+          selectedTab={selectedTab}
+        />
+        <TodoBar
+          tab={tab}
+          text="Completed"
+          design="py-2 text-center w-1/3 rounded-br-lg"
+          selectedTab={selectedTab}
+        />
+      </div>
     </div>
   );
 }
